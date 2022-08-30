f34517ae8637bebaee2f07e3f70b261422f3fb38
Oleksii Martseniuk <martsenyk.l@gmail.com>
2022-08-30 11:25:03 +0300

    create model post

---
+++ fastapi_blog/blog/models.py
@@ -0,0 +1,17 @@
+from sqlalchemy import Column, Integer, String, Text, DateTime
+from sqlalchemy.sql import func
+
+from db.database import Base
+
+
+class Post(Base):
+    __tablename__ = 'posts'
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, nullable=False)
+    body = Column(Text, nullable=False)
+    # Автоматически создаст временную метку на стороне базы данных
+    created = Column(DateTime(timezone=True), server_default=func.now())
+    # Изменит временную метку при обновлении на стороне базы данных
+    updated = Column(DateTime(timezone=True), onupdate=func.now())
+    status = Column(String, nullable=False)
