0605cf1f3852e3ca9377efc5acd86bea5173821e
Oleksii Martseniuk <martsenyk.l@gmail.com>
2022-08-30 11:53:54 +0300

    edit filed default

--- fastapi_blog/user/models.py
+++ fastapi_blog/user/models.py
@@ -14,7 +14,7 @@ class User(Base):
     email = Column(String, unique=True)
     # Автоматически создаст временную метку на стороне базы данных
     created = Column(DateTime(timezone=True), server_default=func.now())
-    is_active = Column(Boolean, nullable=False)
-    is_admin = Column(Boolean, nullable=False)
+    is_active = Column(Boolean, default=True, nullable=False)
+    is_admin = Column(Boolean, default=False, nullable=False)
 
     posts = relationship('Post', back_populates='owner')
