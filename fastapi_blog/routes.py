ba1bbf7b30103daabf19363e81676c361292ae8f
Oleksii Martseniuk <martsenyk.l@gmail.com>
2022-09-02 22:42:39 +0300

    add router post

--- fastapi_blog/routes.py
+++ fastapi_blog/routes.py
@@ -2,6 +2,7 @@ from fastapi import APIRouter
 
 from fastapi_blog.auth.api import auth_router
 from fastapi_blog.user.api import user_router
+from fastapi_blog.blog.endpoint.post import post_router
 
 
 routes = APIRouter()
@@ -9,3 +10,4 @@ routes = APIRouter()
 
 routes.include_router(user_router, prefix='/user', tags=['user'])
 routes.include_router(auth_router, prefix='/auth', tags=['auth'])
+routes.include_router(post_router, prefix='/post', tags=['post'])
