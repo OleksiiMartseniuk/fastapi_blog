e751f84fd7b4d35ecf2dd8c09aabab23036acb67
Oleksii Martseniuk <martsenyk.l@gmail.com>
2022-09-03 22:50:00 +0300

    add service comment

--- fastapi_blog/blog/service.py
+++ fastapi_blog/blog/service.py
@@ -1,5 +1,5 @@
 from fastapi_blog.base.crud import CRUD
-from fastapi_blog.db.base import Post
+from fastapi_blog.db.base import Post, Comment
 
 from . import schemas
 
@@ -8,4 +8,9 @@ class PostService(CRUD[Post, schemas.CreatePost, schemas.UpdatePost]):
     pass
 
 
+class CommentService(CRUD[Comment, schemas.CreateComment, schemas.UpdateComment]):
+    pass
+
+
+service_comment = CommentService(Comment)
 service_post = PostService(Post)
