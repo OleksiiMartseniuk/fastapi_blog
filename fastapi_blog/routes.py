66cf2ff37caa4f20f984c63a91078ca88390b01f
Oleksii Martseniuk <martsenyk.l@gmail.com>
2022-09-03 22:40:29 +0300

    add routes comment

--- fastapi_blog/routes.py
+++ fastapi_blog/routes.py
@@ -3,6 +3,7 @@ from fastapi import APIRouter
 from fastapi_blog.auth.api import auth_router
 from fastapi_blog.user.api import user_router
 from fastapi_blog.blog.endpoint.post import post_router
+from fastapi_blog.blog.endpoint.comment import comment_router
 
 
 routes = APIRouter()
@@ -11,3 +12,4 @@ routes = APIRouter()
 routes.include_router(user_router, prefix='/user', tags=['user'])
 routes.include_router(auth_router, prefix='/auth', tags=['auth'])
 routes.include_router(post_router, prefix='/post', tags=['post'])
+routes.include_router(comment_router, prefix='/comment', tags=['comment'])
