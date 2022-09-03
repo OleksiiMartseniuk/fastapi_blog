bb5bc8f6ab2b6501f6fce5167dfb29bcf95706e9
Oleksii Martseniuk <martsenyk.l@gmail.com>
2022-09-03 19:17:36 +0300

    add  update_post

--- fastapi_blog/blog/endpoint/post.py
+++ fastapi_blog/blog/endpoint/post.py
@@ -23,7 +23,7 @@ def create_post(
     return service_post.create(db, post, owner_id=user.id, status='publish')
 
 
-@post_router.get('/', response_model=schemas.PostResponse)
+@post_router.get('/', response_model=schemas.Post)
 def get_post(
     id: int,
     db: Session = Depends(get_db)
@@ -34,3 +34,13 @@ def get_post(
 @post_router.get('/all/', response_model=list[schemas.PostResponse])
 def all_post(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     return service_post.all(db, skip, limit)
+
+
+@post_router.put('/', response_model=schemas.UpdatePost)
+def update_post(
+    id: int,
+    post: schemas.UpdatePost,
+    db: Session = Depends(get_db),
+    user: User = Depends(get_current_active_user)
+):
+    return service_post.update(db, id, post, owner_id=user.id)
