118120cb378d5c1a2bac097496de9153e5f89cde
Oleksii Martseniuk <martsenyk.l@gmail.com>
2022-09-03 22:39:56 +0300

    create schema comment

--- fastapi_blog/blog/schemas.py
+++ fastapi_blog/blog/schemas.py
@@ -5,6 +5,13 @@ from datetime import datetime
 from fastapi_blog.user.schemas import UserName
 
 
+class CommentId(BaseModel):
+    id: int
+
+    class Config:
+        orm_mode = True
+
+
 class Post(BaseModel):
     id: int
     title: str
@@ -13,7 +20,7 @@ class Post(BaseModel):
     updated: datetime | None
     status: str
     owner: UserName
-    comments: list[int] = []
+    comments: list[CommentId]
     tags: list[int] = []
 
     class Config:
@@ -43,5 +50,33 @@ class PostResponse(BaseModel):
         orm_mode = True
 
 
+class PostTitle(BaseModel):
+    title: str
+
+    class Config:
+        orm_mode = True
+
+
 class Info(BaseModel):
     message: str
+
+
+class Comment(BaseModel):
+    id: int
+    body: str
+    created: datetime
+    updated: datetime | None
+    active: bool
+    owner: UserName
+    post: PostTitle
+
+    class Config:
+        orm_mode = True
+
+
+class CreateComment(BaseModel):
+    body: str
+
+
+class UpdateComment(BaseModel):
+    body: str
