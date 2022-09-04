d3ec0db8c59e9e35d3e58240115e333a36ef51fe
Oleksii Martseniuk <martsenyk.l@gmail.com>
2022-09-04 12:24:32 +0300

    add service tag

--- fastapi_blog/blog/service.py
+++ fastapi_blog/blog/service.py
@@ -1,5 +1,5 @@
 from fastapi_blog.base.crud import CRUD
-from fastapi_blog.db.base import Post, Comment
+from fastapi_blog.db.base import Post, Comment, Tag
 
 from . import schemas
 
@@ -12,5 +12,10 @@ class CommentService(CRUD[Comment, schemas.CreateComment, schemas.UpdateComment]
     pass
 
 
+class TagService(CRUD[Tag, schemas.CreateTag, schemas.UpdateTag]):
+    pass
+
+
+service_tag = TagService(Tag)
 service_comment = CommentService(Comment)
 service_post = PostService(Post)
