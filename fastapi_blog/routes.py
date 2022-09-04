052cbbad18be1f0b8ef3cf81000b60c883432760
Oleksii Martseniuk <martsenyk.l@gmail.com>
2022-09-04 12:25:08 +0300

    add routes tag

--- fastapi_blog/routes.py
+++ fastapi_blog/routes.py
@@ -4,6 +4,7 @@ from fastapi_blog.auth.api import auth_router
 from fastapi_blog.user.api import user_router
 from fastapi_blog.blog.endpoint.post import post_router
 from fastapi_blog.blog.endpoint.comment import comment_router
+from fastapi_blog.blog.endpoint.tag import tag_router
 
 
 routes = APIRouter()
@@ -13,3 +14,4 @@ routes.include_router(user_router, prefix='/user', tags=['user'])
 routes.include_router(auth_router, prefix='/auth', tags=['auth'])
 routes.include_router(post_router, prefix='/post', tags=['post'])
 routes.include_router(comment_router, prefix='/comment', tags=['comment'])
+routes.include_router(tag_router, prefix='/tag', tags=['tag'])
